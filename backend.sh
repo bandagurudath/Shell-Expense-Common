f9ffc2af40967301becde7481f68eecf95514baa
gurudath <gurudath_daws>
2025-08-07 23:13:31 +0530

    commit

--- backend.sh
+++ backend.sh
@@ -1,32 +1,8 @@
 #!/bin/bash
 
-USERID=$(id -u)
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGPATH=/tmp/$SCRIPTNAME-$TIMESTAMP.log
-mysql_root_user=root
+source ./common.sh
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-validate(){
-if [ $1 -eq 0 ]
-then
-echo "$2 ...... $G Success $N"
-else
-echo "$2 ...... $R Failure $N"
-fi
-}
-
-if [ $USERID -eq 0]
-then
-echo "You are super user"
-else 
-echo "you are not super user" 
-exit 1
-fi
+root
 
 echo "Enter mysql root password"
 read mysql_root_password
--- frontend.sh
+++ frontend.sh
@@ -1,30 +1,8 @@
-USERID=$(id -u)
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGPATH=/tmp/$SCRIPTNAME-$TIMESTAMP.log
-mysql_root_user=root
+#!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
 
-validate(){
-if [ $1 -eq 0 ]
-then
-echo "$2 ...... $G Success $N"
-else
-echo "$2 ...... $R Failure $N"
-fi
-}
-
-if [ $USERID -eq 0]
-then
-echo "You are super user"
-else 
-echo "you are not super user" 
-exit 1
-fi
+root()
 
 dnf install nginx -y &>>$LOGPATH
 validate $? "installing nginx"
